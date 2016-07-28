72f651827530e2cbeb60fca8a3d2ddc26045bdb8
Juan Manuel Pérez <jmperez.85@gmail.com>
2016-07-28 00:26:18 -0300

    First step of Porter

--- nlptools/stemming.py
+++ nlptools/stemming.py
@@ -1,4 +1,5 @@
 import re
+import operator
 
 class Rule:
     def __init__(self, suffix, replacement):
@@ -6,20 +7,35 @@ class Rule:
         self.replacement = replacement
 
     def match_length(self, word):
-        match = re.match(self.suffix)
+        match = re.search(self.suffix, word)
         if match:
-            return match.end() - match.start() + 1
+            return match.end() - match.start()
         else:
             return 0
 
     def __call__(self, word):
         return re.sub(self.suffix, self.replacement, word)
 
+class SetOfRules:
+    def __init__(self, rules):
+        self.rules = rules
+
+    def apply(self, word):
+        matches = list(map(lambda r: r.match_length(word), self.rules))
+
+        best_match_index, match_length = max(enumerate(matches), key=operator.itemgetter(1))
+
+        if match_length > 0:
+            word = self.rules[best_match_index](word)
+
+        return word
+
 
 def porter(word):
-    rules = [Rule("ies", "i"), Rule("s", "")]
+    rules = SetOfRules([
+        Rule("sses", "ss"),
+        Rule("ies", "i"),
+        Rule("ss", "ss"),
+        Rule("s", "")])
 
-    for rule in rules:
-        word = rule(word)
-
-    return word
+    return rules.apply(word)
--- tests/porter_test.py
+++ tests/porter_test.py
@@ -8,6 +8,8 @@ class StemmingTest(unittest.TestCase):
     def test_ponies_maps_to_poni(self):
         self.assertEqual(porter("ponies"), "poni")
 
+    def test_caresses_maps_to_caress(self):
+        self.assertEqual(porter("caresses"), "caress")
 
 class RuleTest(unittest.TestCase):
     def test_a_word_that_doesnt_finish_with_ies(self):
@@ -19,6 +21,17 @@ class RuleTest(unittest.TestCase):
         rule = Rule("ies", "i")
 
         self.assertEqual(rule("flies"), "fli")
+
+    def test_match_length_for_a_non_matching_word(self):
+        rule = Rule("ies", "i")
+
+        self.assertEqual(rule.match_length("foo"), 0)
+
+    def test_match_length_for_a_non_matching_word(self):
+        rule = Rule("ies", "i")
+
+        self.assertEqual(rule.match_length("flies"), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
